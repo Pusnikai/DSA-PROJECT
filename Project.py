3fdc7e0c324ae4fb864cd438afc6102e3a240234
Prashanth Nallathamby <prashanth.baskaran101@gmail.com>
2024-03-27 19:35:45 -0400

    Meow

--- Project.py
+++ Project.py
@@ -2,8 +2,27 @@
 INFR 2820U: Algorithm and Data Structures
 Group Project
 Prashanth Baskaran Nallathamby 100784991
-    
+Sangeethan Thevathasan 100867103
+Ayaan Bajwa 100864399
+
 '''
 
 import csv
+import networkx as nx
+
+def load_csv(filepath):
+    Graph = nx.Graph()
+    with open(filepath, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            source = row['Node']
+            target = row['Edge']
+            distance = int(row['Distance'])
+            Graph.add_edge(source, target, weight=distance)
+    return Graph
+
+filepath = 'data.csv'
+Graph = load_csv(filepath)
+print('Nodes: ', Graph.nodes())
+print('Edges: ',Graph.edges())
 
