d841c07c28a953d8691c5c8839b3df2ddabc5522
Prashanth Nallathamby <prashanth.baskaran101@gmail.com>
2024-03-28 23:50:44 -0400

    DONE AND DONE

--- Project.py
+++ Project.py
@@ -45,15 +45,61 @@ def IsChargingStation(Graph, stations):
     
 def DijkstraRouting(Graph, origin, station):
     Visit = {node: False for node in Graph.nodes()}
+    distance = {node:float('inf')for node in Graph.nodes()}
+    path = {node:None for node in Graph.nodes()}
+    distance[origin] = 0
+    
+    while not Visit[station]:
+        minDist = float('inf')
+        minNode = None
+        for node in Graph.nodes():
+            if not Visit[node] and distance[node] < minDist:
+                minDist = distance[node]
+                minNode = node
+                
+        Visit[minNode] = True
+        
+        for neighbor in Graph.neighbors(minNode):
+            if not Visit[neighbor]:
+                newDistance = distance[minNode] + Graph[minNode][neighbor]['weight']
+                if newDistance < distance[neighbor]:
+                    distance[neighbor] = newDistance
+                    path[neighbor] = minNode
+    
+    PathToGoal = []
+    TotalDist = [distance[station]]
+    actualNode = station
+    while actualNode != origin:
+        PathToGoal.insert(0,actualNode)
+        actualNode = path[actualNode]
+    PathToGoal.insert(0,origin)    
+      
+    return PathToGoal, TotalDist
+    
+def EfficientRoute(Graph,origin,stations):
+    optimal = []
+    for station in stations:
+        path, dist = DijkstraRouting(Graph,origin,station)
+        print("\nThe shortest route from {} to {}: ".format(origin,station),path)
+        print('Total Distance is :',dist)
+        optimal.append( (path, dist) )
+    optimal.sort(key=lambda x:x[1]) 
+    ShortestPath,ShortestDist   = optimal[0]
+    print('\nThe Recommended Station Route is : ',ShortestPath)
+    print('Total Distance: ',ShortestDist)
+        
+     
+        
+    
 def main():   
     filepath = 'data.csv'
     Graph = load_csv(filepath)
     stations = ['H','K','Q','T']
     Label = IsChargingStation(Graph,stations)
     Display_Graph(Graph,Label)
-    
-    origin  = input("Enter the current point (Between A to W): ")
-    
+    origin  = input("\nEnter the current point (Between A to W): ")
+    EfficientRoute(Graph,origin,stations)
+   
     
     #print('\nNodes: ', Graph.nodes())
     #print('\nEdges: ',Graph.edges())
