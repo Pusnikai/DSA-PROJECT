0ac0d90a2e594e02eaec0562e7ceae56230226d0
Prashanth Nallathamby <prashanth.baskaran101@gmail.com>
2024-03-29 00:30:58 -0400

    DONEgit add .git add .

--- Project.py
+++ Project.py
@@ -6,12 +6,12 @@ Sangeethan Thevathasan 100867103
 Ayaan Bajwa 100864399
 Abishan Jeyanathan 
 '''
-
+# Importing Required Libraries
 import csv
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
+# Reading the CSV file
 def load_csv(filepath):
     Graph = nx.Graph()
     with open(filepath, 'r') as file:
@@ -23,6 +23,7 @@ def load_csv(filepath):
             Graph.add_edge(source, target, weight=distance)
     return Graph
 
+# Drawing the graph using NetworkX library
 def Display_Graph(Graph,Node_Label):
     pos = nx.spring_layout(Graph)
     nx.draw_networkx_nodes(Graph, pos, node_color=Node_Label)
@@ -31,41 +32,37 @@ def Display_Graph(Graph,Node_Label):
     edge_labels = {(Start,Next): weight['weight'] for Start, Next, weight in Graph.edges(data=True)}
     nx.draw_networkx_edge_labels(Graph, pos, edge_labels=edge_labels)
     plt.show()
-    
+
+# Checking if the Node iterating is a EV station or not
 def IsChargingStation(Graph, stations):
-    
     Node_Label = []
     for node in Graph.nodes():
         if node not in stations:
             Node_Label.append('lightgrey')
         else:
-            Node_Label.append('lightgreen')
-            
+            Node_Label.append('lightgreen')      
     return Node_Label          
-    
+
+# Dijkstra's Algorithm Implementation
 def DijkstraRouting(Graph, origin, station):
     Visit = {node: False for node in Graph.nodes()}
     distance = {node:float('inf')for node in Graph.nodes()}
     path = {node:None for node in Graph.nodes()}
     distance[origin] = 0
-    
     while not Visit[station]:
         minDist = float('inf')
         minNode = None
         for node in Graph.nodes():
             if not Visit[node] and distance[node] < minDist:
                 minDist = distance[node]
-                minNode = node
-                
+                minNode = node   
         Visit[minNode] = True
-        
         for neighbor in Graph.neighbors(minNode):
             if not Visit[neighbor]:
                 newDistance = distance[minNode] + Graph[minNode][neighbor]['weight']
                 if newDistance < distance[neighbor]:
                     distance[neighbor] = newDistance
                     path[neighbor] = minNode
-    
     PathToGoal = []
     TotalDist = [distance[station]]
     actualNode = station
@@ -73,9 +70,9 @@ def DijkstraRouting(Graph, origin, station):
         PathToGoal.insert(0,actualNode)
         actualNode = path[actualNode]
     PathToGoal.insert(0,origin)    
-      
     return PathToGoal, TotalDist
-    
+ 
+#  Determining the Efficient Route and Displaying all the EV station Route
 def EfficientRoute(Graph,origin,stations):
     optimal = []
     for station in stations:
@@ -86,11 +83,9 @@ def EfficientRoute(Graph,origin,stations):
     optimal.sort(key=lambda x:x[1]) 
     ShortestPath,ShortestDist   = optimal[0]
     print('\nThe Recommended Station Route is : ',ShortestPath)
-    print('Total Distance: ',ShortestDist)
-        
-     
-        
-    
+    print('Total Distance: ',ShortestDist)  
+
+# Main Function
 def main():   
     filepath = 'data.csv'
     Graph = load_csv(filepath)
@@ -102,6 +97,7 @@ def main():
     #print('\nNodes: ', Graph.nodes())
     #print('\nEdges: ',Graph.edges())
     #print('\nWeight: ',[(u, v, d['weight']) for u, v, d in Graph.edges(data=True)])
-
+    
 main()
+    
     
